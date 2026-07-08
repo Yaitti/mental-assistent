ab50abd651c3576eb109c6894124e73995e852d7
Yaitti <timofeynikanorov@gmail.com>
2026-07-09 00:32:38 +0300

    feat: add config dataclass and loader

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,42 @@
 import json
 import os
+import yaml
+from dataclasses import dataclass
+
+
+@dataclass
+class BaseConfig:
+	api_key: str
+	base_url: str
+	dialogue_path: str
+	model_config_path: str
+
+@dataclass
+class ModelConfig:
+	model: str = "claude-sonnet-4-6"
+	max_tokens: int = 20
+
+
+def open_config(path):
+	if os.path.exists(path):
+		print("config opened")
+		if path.endswith('.yaml'):
+			print("config is yaml")
+			with open(path, "r", encoding="utf-8") as f:
+				config = yaml.load(f, Loader=yaml.BaseLoader)
+				return config
+		elif path.endswith(".json"):
+			with open(path, "r", encoding="utf-8") as f:
+				config = json.load(path)
+			return config
+		else:
+			print("Incorrect config type")
+	else:
+		print("path doesn't exist")
+
+
+def load_config(path):
+	return BaseConfig(**open_config(path))
 
 
 def read_dialogue_history(path):
