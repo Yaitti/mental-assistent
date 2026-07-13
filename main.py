f031fe006475d65983ba73eb0a54bae8b92fe08d
Yaitti <timofeynikanorov@gmail.com>
2026-07-14 00:22:41 +0300

    refactor: use config instead of hardcode

--- main.py
+++ main.py
@@ -1,56 +1,67 @@
-from urllib import response
-
 import anthropic
 import argparse
 import json
 import os
 
-from httpx import request
+from src import load_config
 
-from src import read_dialogue_history
 
 class MemorableModel:
-	def __init__(self, dialogue_path):
-		self.dialogue_path = dialogue_path
-		self.messages = read_dialogue_history(dialogue_path)
-		self.client = anthropic.Anthropic(
-			api_key="sk-kw9chzS5Y4JfNnZZvG0yB3wMxGOQnffn",
-			base_url="https://api.proxyapi.ru/anthropic"
-		)
+    def __init__(self, config_path="configs/config.yaml"):
+        self.config = load_config(config_path)
+        self.messages = self.read_dialogue()
+        self.client = anthropic.Anthropic(
+            api_key=self.config.api_key,
+            base_url=self.config.base_url
+        )
+        self.model_config = load_config(self.config.model_config_path,
+                                        config_type="model")
 
-	def make_response(self):
-		message = self.client.messages.create(
-			model="claude-sonnet-4-6",
-			max_tokens=1000,
-			messages=self.messages
-		)
+    def read_dialogue(self):
+        if os.path.exists(self.config.dialogue_path):
+            print("dialogue_path --- exists")
+            with open(self.config.dialogue_path, 'r', encoding="utf-8") as f:
+                dialogue = json.load(f)
+                return dialogue
+        else:
+            return []
 
-		return message.content[0].text
+    def make_response(self):
+        message = self.client.messages.create(
+            model=self.model_config.model,
+            max_tokens=self.model_config.max_tokens,
+            messages=self.messages
+        )
 
+        return message.content[0].text
 
-	def dialogue_step(self, request_text: str):
-		request = {"role": "user", "content": request_text}
-		self.messages.append(request)
+        return "test response"
 
-		response = self.make_response()
-		response_dict = {"assistant": "user", "content": response}
-		self.messages.append(response_dict)
+    def dialogue_step(self, request_text: str):
+        request = {"role": "user", "content": request_text}
+        self.messages.append(request)
 
-		with open(self.dialogue_path, 'w', encoding="utf-8") as f:
-			json.dump(self.messages, f)
+        response = self.make_response()
+        response_dict = {"assistant": "user", "content": response}
+        self.messages.append(response_dict)
 
-		return response
+        with open(self.config.dialogue_path, 'w', encoding="utf-8") as f:
+            json.dump(self.messages, f)
+
+        return response
+
 
 def main():
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument('-r', '--request', type=str)
-	args = parser.parse_args()
-	request = args.request
-	session = MemorableModel("data/messages.json")
+    parser.add_argument('-r', '--request', type=str)
+    args = parser.parse_args()
+    request = args.request
+    session = MemorableModel()
 
-	response = session.dialogue_step(request)
-	print(response)
+    response = session.dialogue_step(request)
+    print(response)
 
-if __name__=="__main__":
-	main()
+
+if __name__ == "__main__":
+    main()
