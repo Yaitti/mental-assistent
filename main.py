03acc3aa172dc20cf1d3cb8d56b60a9a3b518fa4
Yaitti <timofeynikanorov@gmail.com>
2026-07-27 19:33:04 +0300

    refractor: rename model class name, variable names and logs text

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ log = logger.set_app_lvl_logger()
 from src import load_config
 
 
-class MemorableModel:
+class MemoryAssistant:
     def __init__(self, config_path="configs/config.yaml"):
         self.config = load_config(config_path)
         self.messages = self.read_dialogue()
@@ -26,22 +26,22 @@ class MemorableModel:
             try:
                 with open(self.config.dialogue_path, 'r', encoding="utf-8") as f:
                     dialogue = json.load(f)
-                    log.info("Dialogue successfully opened")
+                    log.info("Dialogue loaded successfully")
                     return dialogue
             except(json.JSONDecodeError, OSError):
                 log.error("Failed to parse dialogue, starting fresh")
                 return []
         else:
-            log.info("Dialogue doesn't exist or Incorrect path")
+            log.info("Dialogue file not found, starting a new dialogue")
             return []
 
-    def make_response(self):
+    def generate_response(self):
         """message = self.client.messages.create(
             model=self.model_config.model,
             max_tokens=self.model_config.max_tokens,
             messages=self.messages
         )
-        log.info("Model gave response")
+        log.info("Model returned response")
         return message.content[0].text"""
 
         return "test response"
@@ -50,12 +50,12 @@ class MemorableModel:
         request = {"role": "user", "content": request_text}
         self.messages.append(request)
 
-        response = self.make_response()
-        response_dict = {"role": "assistant", "content": response}
+        response = self.generate_response()
+        assistant_message = {"role": "assistant", "content": response}
         self.messages.append(response_dict)
 
         with open(self.config.dialogue_path, 'w', encoding="utf-8") as f:
-            json.dump(self.messages, f)
+            json.dump(self.messages, f, ensure_ascii=False, indent=4)
 
         return response
 
@@ -63,10 +63,10 @@ class MemorableModel:
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-r', '--request', type=str)
+    parser.add_argument('-r', '--request', type=str, required=True)
     args = parser.parse_args()
     request = args.request
-    session = MemorableModel()
+    session = MemoryAssistant()
 
     response = session.dialogue_step(request)
     print(response)
