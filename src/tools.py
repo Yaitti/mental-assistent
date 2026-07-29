9e363cbcccc62c84efde455663b2771990d04978
Yaitti <timofeynikanorov@gmail.com>
2026-07-29 21:42:08 +0300

    fix(tools): fix json not being written in remember_fact

--- src/tools.py
+++ src/tools.py
@@ -6,24 +6,27 @@ import os
 log = logger.get_logger(__name__)
 
 
-def remember_fact(name, content, save_path):
-    fact = {name: content}
+def remember_fact(fact, category, save_path='data/remembered_facts.json'):
+    fact = {category: fact}
     if not os.path.exists(save_path):
-        log.info("No remembered facts file found, starting fresh")
-        facts = []
+        log.info("No remembered facts file found, starting fresh: %s", save_path)
+        facts = {category:fact}
+        with open(save_path, 'w', encoding='utf-8') as f:
+            json.dump(facts, f, ensure_ascii=False, indent=4)
+        return True
     else:
         try:
-            with open(save_path, 'w', encoding='utf-8') as f:
+            with open(save_path, 'r', encoding='utf-8') as f:
                 facts = json.load(f)
-                log.info("Loaded facts from %s", save_path)
+                log.info("Loaded facts from: %s", save_path)
         except(json.JSONDecodeError, OSError):
             facts = []
             log.exception("Failed to parse remembered facts, starting new")
 
-    facts.append(fact)
-    print(facts)
+
     try:
-        with open(save_path, 'r', encoding='utf-8') as f:
+        facts[category] = fact
+        with open(save_path, 'w', encoding='utf-8') as f:
             json.dump(facts, f, ensure_ascii=False, indent=4)
         return True
     except TypeError:
